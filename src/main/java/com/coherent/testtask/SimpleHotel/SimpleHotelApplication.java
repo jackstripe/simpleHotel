26e905490692be19f79dcc3b16ed2d8ff45e83ab
jackstripe <ingcesarfl@gmail.com>
2023-03-22 02:02:53 -0600

    First commit, Spring initializr

---
+++ src/main/java/com/coherent/testtask/SimpleHotel/SimpleHotelApplication.java
@@ -0,0 +1,13 @@
+package com.coherent.testtask.SimpleHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coherent/testtask/SimpleHotel/SimpleHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coherent.testtask.SimpleHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
